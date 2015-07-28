87f107498d882ed2ee55f237a1032e95b05b790d
Ye Mao <cnkmym@gmail.com>
2015-07-28 23:51:18 +0900

    finish unique binary search tree

--- LeetCodeProgram/src/main/java/uniquebinarysearchtree/Solution.java
+++ LeetCodeProgram/src/main/java/uniquebinarysearchtree/Solution.java
@@ -1,126 +1,25 @@
 package uniquebinarysearchtree;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.Stack;
-
-import utility.TreeNode;
-
 public class Solution {
-	int[] digits;
-	public List<TreeNode> paths;
-	public Set<String> pathSet;
+	int[] dp = new int[10000];
 
-	public List<TreeNode> generateTrees(int n) {
-		digits = new int[n];
-		paths = new ArrayList<TreeNode>();
-		pathSet = new HashSet<String>();
-
-		if (n == 0) {
-			paths.add(null);
-			return paths;
-		}
-		for (int i = 0; i < n; i++) {
-			digits[i] = i + 1;
-		}
-
-		computeFullCombination(digits, 0);
-
-		return paths;
+	public Solution() {
+		dp[0] = 1;
 	}
 
-	public String print(TreeNode root) {
-		TreeNode current = root;
-		StringBuilder sb = new StringBuilder();
-		Stack<TreeNode> waiting = new Stack<TreeNode>();
-		while (current != null) {
-			sb.append(current.val);
-			sb.append(",");
-			if (current.left != null) {
-				if (current.right != null) {
-					waiting.add(current.right);
-				}
-				current = current.left;
-				continue;
-			}
-			if (current.right != null) {
-				current = current.right;
-				continue;
-			}
-			current = (waiting.isEmpty()) ? null : waiting.pop();
+	public int numTrees(int n) {
+		if (dp[n] > 0) {
+			return dp[n];
 		}
-		return sb.toString();
+		dp[n] = 0;
+		for (int i = 1; i <= n; i++) {
+			dp[n] += numTrees(n, i);
+		}
+		return dp[n];
 	}
 
-	private void addAsLeftTree(TreeNode root, int val) {
-		if (root.left == null) {
-			TreeNode child = new TreeNode(val);
-			root.left = child;
-			return;
-		}
-		int left = root.left.val;
-		if (val <= left) {
-			addAsLeftTree(root.left, val);
-		} else {
-			addAsRightTree(root.left, val);
-		}
-	}
-
-	private void addAsRightTree(TreeNode root, int val) {
-		if (root.right == null) {
-			TreeNode child = new TreeNode(val);
-			root.right = child;
-			return;
-		}
-		int right = root.right.val;
-		if (val <= right) {
-			addAsLeftTree(root.right, val);
-		} else {
-			addAsRightTree(root.right, val);
-		}
-	}
-
-	private void checkAndMakeTree(int[] digits) {
-		TreeNode root = null;
-		for (int i = 0; i < digits.length; i++) {
-			if (root == null) {
-				root = new TreeNode(digits[i]);
-				continue;
-			}
-			if (digits[i] <= root.val) {
-				addAsLeftTree(root, digits[i]);
-			} else {
-				addAsRightTree(root, digits[i]);
-			}
-		}
-		String pathString = print(root);
-		if (!pathSet.contains(pathString)) {
-			pathSet.add(pathString);
-			paths.add(root);
-		}
-	}
-
-	private void computeFullCombination(int[] digits, int from) {
-		if (from == digits.length - 1) {
-			checkAndMakeTree(digits);
-		}
-		for (int i = from; i < digits.length; i++) {
-			if (i > from) {
-				swap(digits, from, i);
-			}
-			computeFullCombination(digits, from + 1);
-			if (i > from) {
-				swap(digits, from, i);
-			}
-		}
-	}
-
-	private void swap(int[] digits, int from, int to) {
-		int tmp = digits[from];
-		digits[from] = digits[to];
-		digits[to] = tmp;
+	private int numTrees(int n, int votex) {
+		return numTrees(n - votex) * numTrees(votex - 1);
 	}
 
 }
--- LeetCodeProgram/src/test/java/uniquebinarysearchtree/MyTest.java
+++ LeetCodeProgram/src/test/java/uniquebinarysearchtree/MyTest.java
@@ -2,11 +2,6 @@ package uniquebinarysearchtree;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-
 import org.junit.Before;
 import org.junit.Test;
 
@@ -18,40 +13,18 @@ public class MyTest {
 		s = new Solution();
 	}
 
-	private String print(Set<String> data) {
-		List<String> list = new ArrayList<String>(data.size());
-		list.addAll(data);
-		Collections.sort(list);
-		return "[" + String.join(";", list) + "]";
-	}
-
-	@Test
-	public void test0() {
-		s.generateTrees(0);
-		assertEquals("[]", print(s.pathSet));
-	}
-	
 	@Test
 	public void test1() {
-		s.generateTrees(1);
-		assertEquals("[1,]", print(s.pathSet));
+		assertEquals(1, s.numTrees(1));
 	}
 
 	@Test
 	public void test2() {
-		s.generateTrees(2);
-		assertEquals("[1,2,;2,1,]", print(s.pathSet));
+		assertEquals(2, s.numTrees(2));
 	}
 
 	@Test
 	public void test3() {
-		s.generateTrees(3);
-		assertEquals("[1,2,3,;1,3,2,;2,1,3,;3,1,2,;3,2,1,]", print(s.pathSet));
-	}
-	
-	@Test
-	public void test4() {
-		s.generateTrees(4);
-		assertEquals("[1,2,3,4,;1,2,4,3,;1,3,2,4,;1,4,2,3,;1,4,3,2,;2,1,3,4,;2,1,4,3,;3,1,2,4,;3,2,1,4,;4,1,2,3,;4,1,3,2,;4,2,1,3,;4,3,1,2,;4,3,2,1,]", print(s.pathSet));
+		assertEquals(5, s.numTrees(3));
 	}
 }
